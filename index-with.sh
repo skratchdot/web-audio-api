f5b0e575cb71b4364c418f916301808c482e614d
skratchdot <git@skratchdot.com>
2021-04-09 10:40:44 -0400

    fixing auto-close typo in script

--- index-with.sh
+++ index-with.sh
@@ -3,21 +3,21 @@
 interfaces="AnalyserNode AudioBuffer AudioBufferSourceNode AudioContext AudioDestinationNode AudioListener AudioNode AudioParam AudioParamMap AudioProcessingEvent AudioScheduledSourceNode AudioWorklet AudioWorkletGlobalScope AudioWorkletNode AudioWorkletProcessor BaseAudioContext BiquadFilterNode ChannelMergerNode ChannelSplitterNode ConstantSourceNode ConvolverNode DelayNode DynamicsCompressorNode GainNode IIRFilterNode MediaElementAudioSourceNode MediaStreamAudioDestinationNode MediaStreamAudioSourceNode MediaStreamTrackAudioSourceNode OfflineAudioCompletionEvent OfflineAudioContext OscillatorNode PannerNode PeriodicWave ScriptProcessorNode StereoPannerNode WaveShaperNode"
 
 for i in $interfaces; do
-  gsed -i -E "s/^<h3 .*[=\"]$i.*$/<h3 interface lt=\"$i\" \/>/" index.bs
+  gsed -i -E "s/^<h3 .*[=\"]$i.*$/<h3 interface lt=\"$i\">/" index.bs
 done
 
 ./compile.sh;
 cp index.html index-with-lt.html
 
 for i in $interfaces; do
-  gsed -i -E "s/^<h3 .*[=\"]$i.*$/<h3 id=\"$i\" \/>/" index.bs
+  gsed -i -E "s/^<h3 .*[=\"]$i.*$/<h3 id=\"$i\">/" index.bs
 done
 
 ./compile.sh;
 cp index.html index-with-id.html
 
 for i in $interfaces; do
-  gsed -i -E "s/^<h3 .*[=\"]$i.*$/<h3 interface lt=\"$i\" id=\"$i\" \/>/" index.bs
+  gsed -i -E "s/^<h3 .*[=\"]$i.*$/<h3 interface lt=\"$i\" id=\"$i\">/" index.bs
 done
 
 ./compile.sh;
@@ -25,7 +25,7 @@ cp index.html index-with-both.html
 
 for i in $interfaces; do
   lcase="$(tr [A-Z] [a-z] <<< "$i")"
-  gsed -i -E "s/^<h3 .*[=\"]$i.*$/<h3 interface lt=\"$lcase\" id=\"$i\" \/>/" index.bs
+  gsed -i -E "s/^<h3 .*[=\"]$i.*$/<h3 interface lt=\"$lcase\" id=\"$i\">/" index.bs
 done
 
 ./compile.sh;
