68c3ca0689d95e6a61aa5f90e569ff5e1e62494a
skratchdot <git@skratchdot.com>
2021-04-09 09:43:05 -0400

    2 more variants

--- index-with.sh
+++ index-with.sh
@@ -15,3 +15,18 @@ done
 
 ./compile.sh;
 cp index.html index-with-id.html
+
+for i in $interfaces; do
+  gsed -i -E "s/^<h3 .*[=\"]$i.*$/<h3 interface lt=\"$i\" id=\"$i\" \/>/" index.bs
+done
+
+./compile.sh;
+cp index.html index-with-both.html
+
+for i in $interfaces; do
+  lcase="$(tr [A-Z] [a-z] <<< "$i")"
+  gsed -i -E "s/^<h3 .*[=\"]$i.*$/<h3 interface lt=\"$lcase\" id=\"$i\" \/>/" index.bs
+done
+
+./compile.sh;
+cp index.html index-with-both-lcase.html
