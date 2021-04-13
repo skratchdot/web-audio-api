eac40a2e7559aa89532f9e9c617a13efbd0da183
skratchdot <git@skratchdot.com>
2021-04-13 18:16:56 -0400

    scripting dictionary updates

--- index-with.sh
+++ index-with.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 
 interfaces="AnalyserNode AudioBuffer AudioBufferSourceNode AudioContext AudioDestinationNode AudioListener AudioNode AudioParam AudioParamMap AudioProcessingEvent AudioScheduledSourceNode AudioWorklet AudioWorkletGlobalScope AudioWorkletNode AudioWorkletProcessor BaseAudioContext BiquadFilterNode ChannelMergerNode ChannelSplitterNode ConstantSourceNode ConvolverNode DelayNode DynamicsCompressorNode GainNode IIRFilterNode MediaElementAudioSourceNode MediaStreamAudioDestinationNode MediaStreamAudioSourceNode MediaStreamTrackAudioSourceNode OfflineAudioCompletionEvent OfflineAudioContext OscillatorNode PannerNode PeriodicWave ScriptProcessorNode StereoPannerNode WaveShaperNode"
-
-for i in $interfaces; do
-  gsed -i -E "s/^<h3 .*[=\"]$i[\" \>].*$/<h3 interface lt=\"$i\">/" index.bs
-done
-
-./compile.sh;
-cp index.html index-with-lt.html
-
-for i in $interfaces; do
-  gsed -i -E "s/^<h3 .*[=\"]$i[\" \>].*$/<h3 id=\"$i\">/" index.bs
-done
-
-./compile.sh;
-cp index.html index-with-id.html
-
-for i in $interfaces; do
-  gsed -i -E "s/^<h3 .*[=\"]$i[\" \>].*$/<h3 interface lt=\"$i\" id=\"$i\">/" index.bs
-done
-
-./compile.sh;
-cp index.html index-with-both.html
+dictionaries="AnalyserOptions AudioBufferOptions AudioBufferSourceOptions AudioContextOptions AudioNodeOptions AudioParamDescriptor AudioProcessingEventInit AudioTimestamp AudioWorkletNodeOptions BiquadFilterOptions ChannelMergerOptions ChannelSplitterOptions ConstantSourceOptions ConvolverOptions DelayOptions DynamicsCompressorOptions GainOptions IIRFilterOptions MediaElementAudioSourceOptions MediaStreamAudioSourceOptions MediaStreamTrackAudioSourceOptions OfflineAudioCompletionEventInit OfflineAudioContextOptions OscillatorOptions PannerOptions PeriodicWaveConstraints PeriodicWaveOptions StereoPannerOptions WaveShaperOptions"
 
 for i in $interfaces; do
   lcase="$(tr [A-Z] [a-z] <<< "$i")"
@@ -29,10 +9,18 @@ for i in $interfaces; do
   gsed -i -E "s/^<h4 .*[=\"]$i[\" \>].*$/<h4 interface lt=\"$lcase\" id=\"$i\">/" index.bs
 done
 
+for i in $dictionaries; do
+  lcase="$(tr [A-Z] [a-z] <<< "$i")"
+  gsed -i -E "s/^<h4 .*[=\"]$i[\" \>].*$/<h4 dictionary lt=\"$lcase\" id=\"$i\">/" index.bs
+  gsed -i -E "s/^<h5 .*[=\"]$i[\" \>].*$/<h5 dictionary lt=\"$lcase\" id=\"$i\">/" index.bs
+done
+
 ./compile.sh;
 cp index.html index-with-both-lcase.html
 
 # print links for pr
+echo ""
+echo "### Interfaces"
 num=0
 for i in $interfaces; do
   lcase="$(tr [A-Z] [a-z] <<< "$i")"
@@ -42,3 +30,16 @@ for i in $interfaces; do
   echo "- https://projects.skratchdot.com/web-audio-api/index-with-both-lcase.html#$i"
   echo "- https://projects.skratchdot.com/web-audio-api/index-with-both-lcase.html#$lcase"
 done
+
+echo ""
+echo "### Dictionaries"
+num=0
+for i in $dictionaries; do
+  lcase="$(tr [A-Z] [a-z] <<< "$i")"
+  num=$((num+1))
+  echo ""
+  echo "#### $num. $i:"
+  echo "- https://projects.skratchdot.com/web-audio-api/index-with-both-lcase.html#$i"
+  echo "- https://projects.skratchdot.com/web-audio-api/index-with-both-lcase.html#$lcase"
+done
+
